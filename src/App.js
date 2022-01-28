fc60bccbbf5153eedf8ad332da4102b33673e733
Saumya Garg <garg.saumya20@gmail.com>
2022-01-28 18:46:27 +0530

    final

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import {Box,makeStyles} from "@material-ui/core";
-import Header from "./components/Header"
-import InfoHeader from "./components/InfoHeader";
+import { Box, makeStyles } from '@material-ui/core';
 
-const useStyles = makeStyles({
-  container:{
+//components
+import Header from './components/Header';
+import InfoHeader from './components/InfoHeader';
+import Articles from './components/Articles';
+
+const useStyles = makeStyles(theme => ({
+  container: {
     marginTop: 110,
     width: '59%',
-    margin: '0 auto'
+    margin: '0 auto',
+    [theme.breakpoints.down('md')]: {
+      width: '75%'
+    },
+    [theme.breakpoints.down('sm')]: {
+      width: '85%'
+    }
   }
-})
+}))
 
 function App() {
-  const classes =useStyles();
+  const classes = useStyles();
+
   return (
     <Box>
-    <Header/>
-    <Box className={classes.container}>
-    <InfoHeader />
-    </Box>
+      <Header />
+      <Box className={classes.container}>
+        <InfoHeader />
+        <Articles />
+      </Box>
     </Box>
   );
 }
 
 export default App;
- 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import { AppBar, Toolbar, makeStyles } from "@material-ui/core";
-import {Menu} from "@material-ui/icons";
-
-
+import { AppBar, Toolbar, makeStyles, Box } from '@material-ui/core';
+import { Menu } from '@material-ui/icons'
 
 const useStyles = makeStyles({
     header: {
-        backgroundColor: "#fff",
+        background: '#fff',
         height: 70
     },
-    logo:{
-        height:55,
-        margin:'auto',
-        paddingRight:70 
+    menu: {
+        color: '#000'
     },
-    menu:{
-        color:"#000"
+    image: {
+        height: 55,
+        margin: 'auto',
+        paddingRight: 70
     }
 })
 
-export default function Header() {
+const Header = () => {
     const classes = useStyles();
     const url = 'https://assets.inshorts.com/website_assets/images/logo_inshorts.png';
-    return <div>
+
+    return (
         <AppBar className={classes.header}>
             <Toolbar>
-                <Menu className={classes.menu}/>
-                <img src={url} alt="logo" className={classes.logo} />
+                <Menu className={classes.menu} />
+                <img src={url} alt="logo" className={classes.image} />
             </Toolbar>
         </AppBar>
-
-    </div>
+    )
 }
+
+export default Header;  
